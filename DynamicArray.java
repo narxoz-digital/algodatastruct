b3f00e0ebdbef460021ba1cb4f338c22bf90711a
Konstantin Latuta <konstantin.latuta@gmail.com>
2024-10-03 16:44:40 +0500

    Update DynamicArray.java
    Update ResizableArray

--- DynamicArray.java
+++ DynamicArray.java
@@ -1,100 +1,117 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Vector;
 
-class DynamicArray{
-    private int [] A; // capacity = A.length [0,0,0,0,0,0] = 5
-    private int size; // [4,5,1, 0, 0] size=3
+class ResizableArray{
+    private int [] A;
+    private int size;
+    public ResizableArray(){
+        size = 0;
+        A = new int[0];
+    }
 
-    @Override
     public String toString() {
         StringBuilder st = new StringBuilder();
         st.append("[");
-        for (int i = 0; i < size-1; i++) {
-            st.append(A[i]+",");
+        if (size>0) st.append(A[0]);
+        for (int i = 1; i < size; i++) {
+            st.append(",");
+            st.append(A[i]);
         }
-        if (size>0) st.append(A[size-1]);
         st.append("]");
         return st.toString();
     }
-
-    public DynamicArray(){
-        this.A = new int[0];
-        this.size = 0;
-    }
-
-    public DynamicArray(int size){
-        if (size<0) throw new RuntimeException("E ti che!");
-        this.A = new int[size];
-        this.size = size;
-    }
-
     public void add(int item){
-        if (size == A.length){
-            if (size==0){
-                size = 1;
-                A = new int[1];
-                A[0] = item;
-            } else{
-                A = Arrays.copyOf(A,size*2);
-//                int []B = new int [size*2];
-//                for (int i = 0; i < A.length; i++) {
-//                    B[i] = A[i];
-//                }
-//                A = B;
-                A[size]=item; // size<A.length
-                size++;
-            }
-        } else{
-            A[size]=item; // size<A.length
-            size++;
+        if (size==0){
+            size=1;
+            A = new int[1];
+            A[size-1] = item;
+            return;
         }
 
+        if (size<A.length){
+            size++;
+            A[size-1]=item;
+            return;
+        }
+
+        if (size==A.length){
+//            int[] B = new int [A.length*2];
+//            for (int i = 0; i < A.length; i++) {
+//                B[i] = A[i];
+//            }
+//            A = B;
+            A = Arrays.copyOf(A,A.length*2);
+            size++;
+            A[size-1]=item;
+            return;
+        }
+    }
+    public void add(int index, int item){
 
     }
-    public void add(int index,int item){
-        /*
-        Inserts the specified element at the specified position in this list.
-         Shifts the element currently at that position (if any) and
-          any subsequent elements to the right (adds one to their indices)
-         */
+    public int removeLast(){
+        int last = A[size-1];
+        size--;
+        if (size==A.length/2) {
+//            int[] B = new int[A.length/2];
+//            for (int i = 0; i < A.length/2; i++) {
+//                B[i] = A[i];
+//            }
+//            A = B;
+            A = Arrays.copyOf(A,A.length/2);
+        }
+        return last;
     }
-    public void remove(int index){} // time O(1), O(n), O(logn)
-    public void removeFirst(int item){} // time
-    public void removeLast(int item){} // time
-
+    public int remove(int index){
+        return 0;
+    }
     public int get(int index){
-        if (index<0 || index >=size) throw new RuntimeException("Eeeeeee!");
-        return A[index];
+        return 0;
     }
     public void set(int index, int item){
 
     }
-    public int size(){ return size; }
-    public int capacity(){ return A.length; }
-
-    public static void main(String[] args) {
-        // Vector<Integer> v = new Vector<>(); // thread safe
-        // ArrayList<Integer> a = new ArrayList<>();
-        // a.add(3,4);
-        DynamicArray d = new DynamicArray();
-        d.add(25);
-        System.out.println(d.size() + " "+ d.capacity()+" "+d);
-        d.add(26);
-        System.out.println(d.size() + " "+ d.capacity()+" "+d);
-        d.add(27);
-        System.out.println(d.size() + " "+ d.capacity()+" "+d);
-        d.add(28);
-        System.out.println(d.size() + " "+ d.capacity()+" "+d);
-        d.add(29);
-        System.out.println(d.size() + " "+ d.capacity()+" "+d);
+    public int size(){
+        return size;
     }
-
+    public int capacity() { return A.length;}
 }
 
+public class Main{
+    public static void main(String[] args) {
+        ResizableArray A = new ResizableArray();
+        System.out.println(A.size()+" "+A+" "+A.capacity());
+        A.add(5);
+        System.out.println(A.size()+" "+A+" "+A.capacity());
+        A.add(7);
+        System.out.println(A.size()+" "+A+" "+A.capacity());
+        A.add(8);
+        System.out.println(A.size()+" "+A+" "+A.capacity());
+        A.add(9);
+        System.out.println(A.size()+" "+A+" "+A.capacity());
+        A.add(4);
+        System.out.println(A.size()+" "+A+" "+A.capacity());
+        A.add(3);
+        System.out.println(A.size()+" "+A+" "+A.capacity());
+        A.add(6);
+        System.out.println(A.size()+" "+A+" "+A.capacity());
+        A.add(2);
+        System.out.println(A.size()+" "+A+" "+A.capacity());
+        A.add(1);
+        System.out.println(A.size()+" "+A+" "+A.capacity());
+        A.removeLast();
+        A.removeLast();
+        A.removeLast();
+        A.removeLast();
+        A.removeLast();
+        A.removeLast();
+        System.out.println(A.size()+" "+A+" "+A.capacity());
 
-/* C++ example:
 
+    }
+}
+
+/*
 // Example program
 #include <iostream>
 #include <vector>
@@ -105,27 +122,23 @@ int main()
 {
   vector<int> v;
   v.push_back(5);
-  v.push_back(6);
-  v.push_back(7);
-  v.push_back(8);
-  v.push_back(9);
-  v.push_back(10);
-
-  for(int i=0;i<v.size();i++) cout << v[i] << endl;
-
-  cout << "size = " <<  v.size() << endl;
-  cout << "capacity = " <<  v.capacity() << endl;
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  v.push_back(5);
+  cout << v.size() << endl;
+  cout << v.capacity() << endl;
 }
-*/
-
-/* output:
-5
-6
-7
-8
-9
-10
-size = 6
-capacity = 8
  */
-
