3218752740183868bb8dcdb9555b2537b7f6db85
Konstantin Latuta <konstantin.latuta@gmail.com>
2024-11-06 16:45:16 +0500

    Update MyHashSet.java
    Update MyHashSet

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,18 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-// debug and run
-public class MyHashSet{
+public class MyHashSet {
     public static void main(String[] args) {
         MyHashSet s = new MyHashSet();
-        s.add(1234567);
-        s.add(3334567);
-        s.add(1234568);
-        s.add(1234000);
-        s.add(1234025);
+        s.add(3234567); // 234567
+        s.add(1234567); // 234567
+        s.add(1234568); // 234568
+        s.add(1234000); // 234000
+        s.add(1234025); // 234025
         System.out.println(Arrays.toString(s.toArray()));
     }
-  
+
     ArrayList<Integer> [] bucket;
     int size;
     public MyHashSet(){
@@ -22,15 +21,16 @@ public class MyHashSet{
 
     public void add(int x){
         int hash = x%1000000;
+        if (bucket[hash]==null) bucket[hash] = new ArrayList<Integer>();
         if (!bucket[hash].contains(x)){
             bucket[hash].add(x);
             size++;
         }
-
     }
 
     public void remove(int x){
         int hash = x%1000000;
+        if (bucket[hash]==null) return;
         int index = bucket[hash].indexOf(x);
         if (index>=0) {
             bucket[hash].remove(index);
@@ -42,7 +42,7 @@ public class MyHashSet{
         int [] array = new int[size];
         int k = 0;
         for(int i=0;i<1000000;i++){
-            if (!bucket[i].isEmpty()){
+            if (bucket[i]!=null && !bucket[i].isEmpty()){
                 for (int j = 0; j < bucket[i].size(); j++) {
                     array[k++] = bucket[i].get(j);
                 }
